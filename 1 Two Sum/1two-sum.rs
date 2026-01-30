3df3835f43153d9f68988f3ba0606b48f92558eb
AJ Bushman <8933768+meepcode@users.noreply.github.com>
2026-01-30 21:53:09 +0000

    Code challenge submission for question: 1 Two Sum

---
+++ 1 Two Sum/1two-sum.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in (i + 1)..nums.len() {
+                if (nums[i] + nums[j]) as i32 == target {
+                    return vec![i as i32, j as i32];
+                } else {
+                    continue
+                }
+            }
+        }
+
+        vec![-1, -1]
+    }
+}
